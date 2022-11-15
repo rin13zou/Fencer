1d59cdbc9bbee5c0e1461135d91bb154c26f0330
MasudaRintarou <112672796+rin13zou@users.noreply.github.com>
2022-11-15 09:07:10 +0000

    ｲｲﾈ機能実装したのでコミット

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,11 @@ class Post < ApplicationRecord
   belongs_to :user
 
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  
+   def favorited_by?(user)
+     favorites.exists?(user_id: user_id)
+   end
 
 end
