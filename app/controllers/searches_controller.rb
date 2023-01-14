207ee65f7455516077ba655aecbfa6fe35d9da99
MasudaRintarou <112672796+rin13zou@users.noreply.github.com>
2023-01-14 09:58:31 +0000

    機能追加のためプッシュ

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,15 +1,15 @@
 class SearchesController < ApplicationController
-  
+
   before_action :authenticate_user!
 
   def search
     @range = params[:range]
 
     if @range == "User"
-      @users = User.looks(params[:word])
+      @users = User.looks(params[:word]).where(is_deleted: false)
     else
       @posts = Post.looks(params[:word])
     end
   end
-  
+
 end
